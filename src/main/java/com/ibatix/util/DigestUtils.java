a11570a1741823e76f338157bffeb2985a451c48
master <ibatix@qq.com>
2022-02-14 13:15:16 +0800

    1. 纠正数据摘要算法为SHA256 2. 文件索引创建测试通过

--- src/main/java/com/ibatix/util/DigestUtils.java
+++ src/main/java/com/ibatix/util/DigestUtils.java
@@ -29,7 +29,7 @@ public final class DigestUtils {
      * @return MD5码
      */
     public static String md5sum(File file) {
-        return digest(file, DigestAlgorithm.MD5);
+        return digest(file, DigestAlgorithm.SHA256);
     }
 
     /**
@@ -39,7 +39,7 @@ public final class DigestUtils {
      * @return MD5码
      */
     public static String md5sum(InputStream in) {
-        return digest(in, DigestAlgorithm.MD5);
+        return digest(in, DigestAlgorithm.SHA256);
     }
 
     /**
